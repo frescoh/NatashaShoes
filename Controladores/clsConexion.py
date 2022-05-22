7881712da5e3fe946f9de7b85c89ee1d8357a02d
Hernan Fresco <herny.12@gmail.com>
2022-05-22 03:23:51 -0300

    hbd

--- Controladores/clsConexion.py
+++ Controladores/clsConexion.py
@@ -2,11 +2,11 @@ import MySQLdb
 import Controladores.Credenciales as c
 
 class clsConexion: 
-        def __init__(self, DB_HOST=c.host, DB_USER=c.user, DB_PASS=c.passw, DB_NAME=c.db):   
-                self.datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
-                self.conn = MySQLdb.connect(*self.datos)
+        def __init__(self,datos=c.datos):   
+                self.datos = datos
+                self.conn = MySQLdb.connect(**self.datos)
                 self.conn.autocommit(True)
-                MySQLdb.connect(*self.datos)
+                MySQLdb.connect(**self.datos)
                 self.cursor = self.conn.cursor()
 
         def run_query(self,query=''):
